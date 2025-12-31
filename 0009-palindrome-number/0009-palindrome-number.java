2d49cd53bdad91cd263e9b58a3515f6baca78adf
Rishabh Sharma <58072640+Rist-hub@users.noreply.github.com>
2025-12-31 14:33:45 +0530

    Time: 5 ms (74.05%), Space: 46 MB (31.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -11,36 +11,25 @@ class Solution {
                 ten = ten*10;
                 ++count;
             }
-             System.out.println("count - "+count);
             compareCount=count/2;
             while(compareCount>0){
                 tempX = x;
                 ten = ten/10;
-                System.out.println("ten - "+ten);
                 tempX=tempX/ten;
-                System.out.println("tempX - "+tempX);
-                System.out.println(tempX%10); //Forward count
                 int fwd = (int)tempX%10;
                 tempX=backwardCount%10;
-                System.out.println(tempX); //back count
                 int back = (int)tempX;
                 backwardCount=backwardCount/10;
                 if(fwd==back){
-                    System.out.println("inside compare if");
                     ++compare;
                 }else if(fwd!=back){
-                    System.out.println("else if");
                     break;
                 }
                 --compareCount;
             }
-            System.out.println("comapare - "+compare);
-            System.out.println("compareCount - "+compareCount);
             if(compare==(count/2)){
-                System.out.println("pal");
                 return true;
             }else{
-                System.out.println("not pal");
                 return false;
             }
         }    

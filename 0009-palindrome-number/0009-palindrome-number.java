7b2d9757f1eb90f27524910765ac10b1c5dad522
Rishabh Sharma <58072640+Rist-hub@users.noreply.github.com>
2025-12-31 14:30:17 +0530

    Time: 513 ms (5.01%), Space: 47.2 MB (7.47%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,37 +1,47 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String strX = Integer.toString(x);
-        int count=0;
-        int lastInd = strX.length();
-        boolean result = false;
-        if(strX.length()%2==0){
-            for(int i=0;i<(strX.length())/2;i++){
-                lastInd = lastInd - 1;
-                if(strX.charAt(i)==strX.charAt(lastInd)){
-                    count=count+1;
+        long tempX = x, count=0, backwardCount=x, compare=0, compareCount=0;
+        long ten = 1; 
+        if(x<0){
+            return false;
+        }else{
+            while(tempX!=0){
+                tempX=tempX/10;
+                ten = ten*10;
+                ++count;
+            }
+             System.out.println("count - "+count);
+            compareCount=count/2;
+            while(compareCount>0){
+                tempX = x;
+                ten = ten/10;
+                System.out.println("ten - "+ten);
+                tempX=tempX/ten;
+                System.out.println("tempX - "+tempX);
+                System.out.println(tempX%10); //Forward count
+                int fwd = (int)tempX%10;
+                tempX=backwardCount%10;
+                System.out.println(tempX); //back count
+                int back = (int)tempX;
+                backwardCount=backwardCount/10;
+                if(fwd==back){
+                    System.out.println("inside compare if");
+                    ++compare;
+                }else if(fwd!=back){
+                    System.out.println("else if");
+                    break;
                 }
+                --compareCount;
             }
-            if(count==(strX.length())/2){
-                result = true;
+            System.out.println("comapare - "+compare);
+            System.out.println("compareCount - "+compareCount);
+            if(compare==(count/2)){
+                System.out.println("pal");
+                return true;
+            }else{
+                System.out.println("not pal");
+                return false;
             }
-            else{
-                result = false;
-            }
-        }
-        else if(strX.length()%2!=0){
-            for(int i=0;i<(strX.length()-1)/2;i++){
-                lastInd = lastInd - 1;
-                if(strX.charAt(i)==strX.charAt(lastInd)){
-                    count=count+1;
-                }
-            }
-            if(count==(strX.length()-1)/2){
-                result = true;
-            }
-            else{
-                result = false;
-            }
-        }
-        return result;
+        }    
     }
 }
